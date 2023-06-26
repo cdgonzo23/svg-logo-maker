f7eadc30a88e7b3e97bc245712340831bdd35c11
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-06-26 15:56:15 -0700

    logo generator is now functional

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const { join } = require('path');
 const { writeFile } = require('fs/promises');
-const { createDocument } = require('./lib/shapes');
+const { createDocument, Circle, Square, Triangle } = require('./lib/shapes');
 
 class CLI {
     constructor() {
@@ -22,24 +22,36 @@ class CLI {
                 {
                     type: 'input',
                     name: 'shapeColor',
-                    message: 'What color would you like your logo to be?',
+                    message: 'What color would you like your logo to be? (If using hex color please include the #)',
                 },
                 {
                     type: 'input',
                     name: 'text',
                     message: 'Enter the text for your logo (3 characters max)',
+                    validate: function (text) {
+                        if (text.length < 3) {
+                            return console.log('Logo text must be 3 characters or less')
+                        }
+                        return true;
+                    }
                 },
                 {
                     type: 'input',
                     name: 'textColor',
-                    message: 'What color would you like your text to be?',
+                    message: 'What color would you like your text to be?  (If using hex color please include the #)',
                 }, 
             ])
             .then(({ text, shape, shapeColor, textColor }) => {
-                this.text = `${text}`
-                this.shape = `${shape}`
-                this.shapeColor = `${shapeColor}`
-                this.textColor = `${textColor}`
+                if (shape === 'circle') {
+                    this.shape = new Circle().render();
+                } else if (shape === 'square') {
+                    this.shape = new Square().render();
+                } else {
+                    this.shape = new Triangle().render();
+                };
+                this.text = `${text}`;
+                this.shapeColor = `${shapeColor}`;
+                this.textColor = `${textColor}`;
             })
             .then(() => {
                 return writeFile(
